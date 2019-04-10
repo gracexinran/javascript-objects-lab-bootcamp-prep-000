2dc111e30f4bb861330386af6019da10b312b06d
Xinran Che <gracexinran@gmail.com>
2019-04-10 04:15:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
   object[key] = value;
   return object;
 }
+function deleteFromObjectByKey(object,key){
+  clone = Object.assign({},object);
+  delete clone[key];
+  return clone;
+}
