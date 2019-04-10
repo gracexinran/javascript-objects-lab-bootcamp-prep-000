01bc647e34edacb1a07c6876acfa362c571b2b24
Xinran Che <gracexinran@gmail.com>
2019-04-10 02:52:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ var recipes = {};
 function updateObjectWithKeyAndValue(object,key,value) {
   return object.assign({},object,{[key]:value});
 }
+function destructivelyUpdateObjectWithKeyAndValue(object,key,valye){
+  object[key] = value;
+  return object;
+}
